215488da58d823fe15eb13d9e506fd392ad93286
Kate <phungthingochien99@gmail.com>
2025-02-20 17:31:05 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'address',
+        'id_country',
+        'avatar',
+        'role',
     ];
 
     /**
