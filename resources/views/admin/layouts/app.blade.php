c821e41e0a6ac3d3cb525b65996b87c4f259d89a
Kate <phungthingochien99@gmail.com>
2025-02-20 22:51:23 +0700

    add layouts for admin

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<!-- <html dir="ltr" lang="en"> -->
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('admin/assets/images/favicon.png')}}">
+    <title>Nice admin Template - The Ultimate Multipurpose admin template</title>
+    <!-- Custom CSS -->
+    <link href="{{asset('admin/css/style.min.css')}}" rel="stylesheet">
+    <link href="{{asset('admin/assets/libs/chartist/dist/chartist.min.css')}}" rel="stylesheet">
+    <script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+<!-- Preloader - style you can find in spinners.css -->
+<div class="preloader">
+    <div class="lds-ripple">
+        <div class="lds-pos"></div>
+        <div class="lds-pos"></div>
+    </div>
+</div>
+<!-- Main wrapper - style you can find in pages.scss -->
+<div id="main-wrapper" data-navbarbg="skin6" data-theme="light" data-layout="vertical" data-sidebartype="full" data-boxed-layout="full">
+    <!-- Topbar header - style you can find in pages.scss -->
+    @include('admin.layouts.header')
+    <!-- End Topbar header -->
+
+    <!-- Left Sidebar - style you can find in sidebar.scss  -->
+    @include('admin.layouts.left-sidebar')
+    <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+
+    <!-- Page wrapper  -->
+    <!-- <div class="page-wrapper"> -->
+    @yield('content')
+    <!-- </div> -->
+
+    <!-- footer -->
+    @include('admin.layouts.footer')
+
+</div>
+
+<!-- All Jquery -->
+<script src="{{asset('admin/assets/libs/jquery/dist/jquery.min.js')}}"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="{{asset('admin/assets/libs/popper.js/dist/umd/popper.min.js')}}"></script>
+<script src="{{asset('admin/assets/libs/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="{{asset('admin/assets/extra-libs/sparkline/sparkline.js')}}"></script>
+<!--Wave Effects -->
+<script src="{{asset('admin/js/waves.js')}}"></script>
+<!--Menu sidebar -->
+<script src="{{asset('admin/js/sidebarmenu.js')}}"></script>
+<!--Custom JavaScript -->
+<script src="{{asset('admin/js/custom.min.js')}}"></script>
+@yield('script')
+</body>
+
+</html>
