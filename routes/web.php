6aca19c5b1e516c00eef208f0991256cfd09f4b7
Kate <phungthingochien99@gmail.com>
2025-02-24 16:01:43 +0700

    group middleware

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Route::middleware(['AdminRole'])->group(function () {
+Route::middleware([\App\Http\Middleware\AdminRole::class])->group(function () {
     Route::get('/admin/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'dashboard'])->name('dashboard');
 
     Route::get('/admin/profile', [App\Http\Controllers\Admin\UserController::class, 'index'])->name('profile');
@@ -23,6 +23,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
     Route::get('/admin/country/{country}', [App\Http\Controllers\Admin\CountryController::class, 'show'])->name('country.show');
     Route::put('/admin/country/{country}', [App\Http\Controllers\Admin\CountryController::class, 'update'])->name('country.update');
     Route::delete('/admin/country/{country}', [App\Http\Controllers\Admin\CountryController::class, 'destroy'])->name('country.destroy');
-//});
+});
 
 
